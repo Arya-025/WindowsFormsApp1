42cd96d001d17c09f8c2fb31314bda04cc2b7123
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-06-20 16:14:03 +0700

    membuat supaya progaram yang jalan pertama kali halaman dashbord

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -9,14 +9,15 @@ namespace WindowsFormsApp1
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+            /// The main entry point for the application.
+            /// </summary>
+            [STAThread]
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Dashbord());
+            }
+        
+    } 
 }
